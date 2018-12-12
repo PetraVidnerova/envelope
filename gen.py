1ca084fa9b7f1362cabfcecfa85db367219721e0
Petra Vidnerová <petra.vidnerova@gmail.com>
2018-12-12 14:18:20 +0100

    new gen

--- gen.py
+++ gen.py
@@ -1,12 +1,12 @@
 import random
 from math import sin
+import numpy as np
 
-for i in range(100):
-    x = i  * 0.10 
-    print(x, sin(x)+random.random())
+for x in np.linspace(-1.0, 1.0, 200):
+    print(x, sin(10*x)+random.random())
 
     
 for i in range(3):
-    x = 1.0 + random.random()
+    x = 0 + 0.1*random.random()
     y = 2.5 + random.random()
     print(x, y)
