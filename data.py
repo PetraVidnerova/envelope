a5e34517299215b20cd7b6a3608b46ad54881596
Petra Vidnerová <petra.vidnerova@gmail.com>
2019-05-16 16:09:43 +0200

    data may have more columns, lower and upper bound can be given

--- data.py
+++ data.py
@@ -1,8 +1,37 @@
 import numpy as np
 
-def load_data(name):
+def select(xo, yo, lower, upper):
+
+    lower = lower.reshape(yo.shape)
+    upper = upper.reshape(yo.shape)
+
+    sel1 = yo > lower
+    sel2 = yo < upper 
+    selected = sel1 & sel2
+
+    x = xo[selected]
+    y = yo[selected]
+        
+    return x, y     
+
+def load_data(name, lower=None, upper=None):
+
     matrix = np.loadtxt(name+".txt")
-    x = matrix[:,0]
-    y = matrix[:,1]
+    x = matrix[:,:-1]
+    y = matrix[:,-1]
+
+
+    if lower is not None and upper is not None:
+
+        lower = lower.reshape(y.shape)
+        upper = upper.reshape(y.shape)
+
+        sel1 = y > lower
+        sel2 = y < upper 
+        selected = sel1 & sel2
+
+        x = x[selected]
+        y = y[selected]
+
     return x, y
     
