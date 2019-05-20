1578f4e70af384d2da1e54019a875401b7a33ebe
Petra Vidnerová <petra.vidnerova@gmail.com>
2019-05-20 10:05:04 +0200

    do not plot, save vectors and models

--- test.py
+++ test.py
@@ -15,14 +15,15 @@ from loss import CustomLoss
 import matplotlib.pyplot as plt
 
 
-seed(42)
-set_random_seed(42)
+#seed(42)
+#set_random_seed(42)
 
 def mlp(loss):
 
     model = Sequential()
-    model.add(Dense(32, input_shape=(1,), activation="sigmoid"))
+    model.add(Dense(32, input_shape=(3,), activation="sigmoid"))
     model.add(Dense(16, activation="sigmoid"))
+#    model.add(Dense(16, activation="sigmoid"))
     model.add(Dense(1))
 
     model.summary()
@@ -52,26 +53,30 @@ def fit_n(n, model_generator, loss, x, y):
     
 if __name__ == "__main__":
 
-    x, y = load_data("data4")
+    x, y = load_data("data_turisti_sel")
 
-    tau1 = float(sys.argv[1])
-    tau2 = float(sys.argv[2])
+    index = sys.argv[1]
+    tau1 = float(sys.argv[2])
+    tau2 = float(sys.argv[3])
     
     # model 1 - upper bound 
     loss = CustomLoss(tau1)
     model = fit_n(5, mlp, loss.loss, x, y)
     ym1 = model.predict(x)
+    model.save("mlp{}_{}.h5".format(index, tau1))
+    np.save("mlp{}_y_{}".format(index, tau1), ym1)
 
     # model 2 - lower bound
     loss = CustomLoss(tau2)
     model = fit_n(5, mlp, loss.loss, x, y)
     ym2 = model.predict(x)
+    model.save("mlp{}_{}.h5".format(index, tau2))
+    np.save("mlp{}_y_{}".format(index, tau2), ym2)
     
-    
-    fig, ax = plt.subplots()
-    ax.scatter(x, y, color='b')
-    ax.plot(x, ym1, color='r')
-    ax.plot(x, ym2, color='r')
-
-    #plt.show()    
-    plt.savefig("mlp_{}_{}.png".format(tau1, tau2), bbox_inches='tight')
+#fig, ax = plt.subplots()
+#ax.scatter(x, y, color='b')
+#ax.plot(x, ym1, color='r')
+#ax.plot(x, ym2, color='r')
+#
+##plt.show()    
+#plt.savefig("mlp_{}_{}.eps".format(tau1, tau2), bbox_inches='tight')
