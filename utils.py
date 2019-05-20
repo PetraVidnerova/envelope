614bc73f202bbcca381539e1122fb8f3990197a7
Petra Vidnerová <petra.vidnerova@gmail.com>
2019-05-20 10:16:57 +0200

    trimmed square error

---
+++ utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+
+def mean_squared_error(x, y):
+    assert len(x) == len(y), "calling MSE on vectors of different size"
+    r = np.square(x - y)
+    return np.sum(r) / len(r)
+
+
+def trimmed_mean_squared_error(x, y, alpha=0.25):
+    assert len(x) == len(y), "calling trimmed loss on vectors of different size"
+    r = np.square(x - y)
+    h = int(math.floor((1 - alpha) * len(x)))
+    idx = np.argpartition(r, h)
+    tr = r[idx[:h]] 
+    return np.sum(tr) / h
