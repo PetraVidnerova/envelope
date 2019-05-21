81a5c0df90785fe562a9c090cf5ec0db24662b2d
Petra Vidnerová <petra.vidnerova@gmail.com>
2019-05-21 14:29:12 +0200

    autompg changes

--- data.py
+++ data.py
@@ -1,25 +1,26 @@
 import numpy as np
 
+
 def select(xo, yo, lower, upper):
 
     lower = lower.reshape(yo.shape)
     upper = upper.reshape(yo.shape)
 
     sel1 = yo > lower
-    sel2 = yo < upper 
+    sel2 = yo < upper
     selected = sel1 & sel2
 
     x = xo[selected]
     y = yo[selected]
-        
-    return x, y     
+
+    return x, y
+
 
 def load_data(name, lower=None, upper=None):
 
     matrix = np.loadtxt(name+".txt")
-    x = matrix[:,:-1]
-    y = matrix[:,-1]
-
+    x = matrix[:, :-1]
+    y = matrix[:, -1]
 
     if lower is not None and upper is not None:
 
@@ -27,11 +28,10 @@ def load_data(name, lower=None, upper=None):
         upper = upper.reshape(y.shape)
 
         sel1 = y > lower
-        sel2 = y < upper 
+        sel2 = y < upper
         selected = sel1 & sel2
 
         x = x[selected]
         y = y[selected]
 
     return x, y
-    
--- test.py
+++ test.py
@@ -1,47 +1,44 @@
 import sys
-from numpy.random import seed
-from tensorflow import set_random_seed
 
 import numpy as np
 
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.optimizers import SGD, RMSprop, Adam
+from keras.optimizers import Adam
 
 
 from data import load_data
 from loss import CustomLoss
 
-import matplotlib.pyplot as plt
 
-
-#seed(42)
-#set_random_seed(42)
+# seed(42)
+# set_random_seed(42)
 
 def mlp(loss):
 
     model = Sequential()
-    model.add(Dense(32, input_shape=(3,), activation="sigmoid"))
-    model.add(Dense(16, activation="sigmoid"))
-#    model.add(Dense(16, activation="sigmoid"))
-    model.add(Dense(1))
+    model.add(Dense(8, input_shape=(4,), activation="tanh"))
+#    model.add(Dense(8, activation="sigmoid"))
+#    model.add(Dense(8, activation="sigmoid"))
+    model.add(Dense(1, activation="tanh"))
 
     model.summary()
 
     model.compile(loss=loss,
-                  optimizer=Adam()
+                  optimizer=Adam(),
                   )
     return model
 
+
 def fit_n(n, model_generator, loss, x, y):
-    """ Fits n models and returns the one with the lowest cost. """ 
-    
-    models = [ model_generator(loss) for _ in range(n) ]
+    """ Fits n models and returns the one with the lowest cost. """
+
+    models = [model_generator(loss) for _ in range(n)]
     losses = []
     for model in models:
         history = model.fit(x, y,
-                            batch_size=128,
-                            epochs=20000,
+                            batch_size=32,
+                            epochs=2000,
                             verbose=1)
         loss_i = history.history["loss"][-1]
         losses.append(loss_i)
@@ -50,33 +47,25 @@ def fit_n(n, model_generator, loss, x, y):
     winner = models[np.argmin(losses)]
     return winner
 
-    
+
 if __name__ == "__main__":
 
-    x, y = load_data("data_turisti_sel")
+    x, y = load_data("data/autompg")
 
     index = sys.argv[1]
     tau1 = float(sys.argv[2])
     tau2 = float(sys.argv[3])
-    
-    # model 1 - upper bound 
+
+    # model 1 - upper bound
     loss = CustomLoss(tau1)
     model = fit_n(5, mlp, loss.loss, x, y)
     ym1 = model.predict(x)
-    model.save("mlp{}_{}.h5".format(index, tau1))
-    np.save("mlp{}_y_{}".format(index, tau1), ym1)
+    model.save("mlp_autompg_{}_{}.h5".format(index, tau1))
+    np.save("mlp_autompg_{}_y_{}".format(index, tau1), ym1)
 
     # model 2 - lower bound
     loss = CustomLoss(tau2)
     model = fit_n(5, mlp, loss.loss, x, y)
     ym2 = model.predict(x)
-    model.save("mlp{}_{}.h5".format(index, tau2))
-    np.save("mlp{}_y_{}".format(index, tau2), ym2)
-    
-#fig, ax = plt.subplots()
-#ax.scatter(x, y, color='b')
-#ax.plot(x, ym1, color='r')
-#ax.plot(x, ym2, color='r')
-#
-##plt.show()    
-#plt.savefig("mlp_{}_{}.eps".format(tau1, tau2), bbox_inches='tight')
+    model.save("mlp_autompg_{}_{}.h5".format(index, tau2))
+    np.save("mlp_autompg_{}_y_{}".format(index, tau2), ym2)
