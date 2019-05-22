323540230712908eb8e79b98b733d184d81501a9
Petra Vidnerová <petra.vidnerova@gmail.com>
2019-05-22 15:39:30 +0200

    use command line arguments

--- fig.py
+++ fig.py
@@ -1,26 +1,39 @@
-import sys
+import click
+import numpy as np
+import matplotlib.pyplot as plt
 
-import numpy as np 
-import matplotlib.pyplot as plt 
-
-from data import load_data 
-
-data_name = sys.argv[1] 
-y_name  = sys.argv[2] 
-x_index = int(sys.argv[3]) 
-
-x, y = load_data(data_name) 
-yp = np.load(y_name) 
+from data import load_data
 
 
-fig, ax = plt.subplots() 
+@click.command()
+@click.option("--save/--show",
+              help='Save the image to file.')
+@click.option("--feature",
+              default=0,
+              help='Index of feature to use in plot.')
+@click.argument("data_name")
+@click.argument("y_name")
+def main(save, feature, data_name, y_name):
 
-print(x.shape, y.shape)
-ax.scatter(x[:, x_index], y, color="b") 
+    x_index = feature
 
-print(x.shape, yp.shape) 
-ax.scatter(x[:, x_index], yp, color="r") 
+    x, y = load_data("data/"+data_name)
+    yp = np.load(y_name)
 
-plt.savefig("{}_{}_{}.eps".format(data_name, x_index, y_name),
-            bbox_inches='tight') 
+    fig, ax = plt.subplots()
 
+    print(x.shape, y.shape)
+    ax.scatter(x[:, x_index], y, color="b")
+
+    print(x.shape, yp.shape)
+    ax.scatter(x[:, x_index], yp, color="r")
+
+    if save:
+        plt.savefig("{}_{}_{}.eps".format(data_name, x_index, y_name),
+                    bbox_inches='tight')
+    else:
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
