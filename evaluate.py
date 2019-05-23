459f5aff723f318f73d3706ceaf066c7d5810b13
Petra Vidnerová <petra.vidnerova@gmail.com>
2019-05-23 12:39:40 +0200

    use click for command line arguments, merge test and test_plain

--- evaluate.py
+++ evaluate.py
@@ -1,22 +1,24 @@
-import sys 
+import sys
 
-import numpy as np 
+import numpy as np
 
-name = sys.argv[1] 
+name = sys.argv[1]
 
-num = 10
+num = 5
 
-plain = [ "{}_{}_plain.txt".format(name, i) for i in range(num) ] 
-trimmed1 = [ "{}_{}_0.1_0.9.txt".format(name, i) for i in range(num) ] 
-trimmed2 = [ "{}_{}_0.2_0.8.txt".format(name, i) for i in range(num) ] 
-trimmed3 = [ "{}_{}_0.3_0.7.txt".format(name, i) for i in range(num) ] 
+plain = ["{}_{}_plain.txt".format(name, i) for i in range(num)]
+trimmed1 = ["{}_{}_0.1_0.9.txt".format(name, i) for i in range(num)]
+trimmed2 = ["{}_{}_0.2_0.8.txt".format(name, i) for i in range(num)]
+trimmed3 = ["{}_{}_0.3_0.7.txt".format(name, i) for i in range(num)]
+
 
 def get_number(line):
     return float(line.split(":")[1])
 
+
 def grep_mse_tmse(file_list):
-    mse = [] 
-    tmse = [] 
+    mse = []
+    tmse = []
     for file_name in file_list:
         with open(file_name, "r") as f:
             for line in f:
@@ -24,10 +26,11 @@ def grep_mse_tmse(file_list):
                     mse.append(get_number(line))
                 elif line.startswith("TMSE"):
                     tmse.append(get_number(line))
-    return mse, tmse 
+    return mse, tmse
+
 
 print(" & MSE & MSE (std) & TMSE & TMSE (std) \\\\")
-for file_list in  plain, trimmed1, trimmed2, trimmed3:
+for file_list in plain, trimmed1, trimmed2, trimmed3:
     mse, tmse = grep_mse_tmse(file_list)
     print(" & {} & {} & {} & {} \\\\".format(np.mean(mse), np.std(mse),
                                              np.mean(tmse), np.std(tmse)))
--- test.py
+++ test.py
@@ -1,31 +1,28 @@
-import sys
-from importlib import import_module 
+from importlib import import_module
 
+import click
 import numpy as np
-
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
+from keras import losses
 
 
 from data import load_data
 from loss import CustomLoss
-
+from utils import mean_squared_error, trimmed_mean_squared_error
 
 # seed(42)
 # set_random_seed(42)
 
 
-
 def fit_n(n, model_generator, loss, x, y):
     """ Fits n models and returns the one with the lowest cost. """
 
-    models = [model_generator(loss) for _ in range(n)]
+    n_inputs = x.shape[1]
+    models = [model_generator(loss, n_inputs) for _ in range(n)]
     losses = []
     for model in models:
         history = model.fit(x, y,
                             batch_size=32,
-                            epochs=2000,
+                            epochs=20000,
                             verbose=1)
         loss_i = history.history["loss"][-1]
         losses.append(loss_i)
@@ -35,28 +32,109 @@ def fit_n(n, model_generator, loss, x, y):
     return winner
 
 
-if __name__ == "__main__":
+@click.group()
+def test():
+    pass
 
-    data_name = "data6"
+
+@test.command()
+@click.option("--label", default="test",
+              help="Label to prepend to output file names.")
+@click.argument("data_name")
+@click.argument("tau1", type=float)
+@click.argument("tau2", type=float)
+@click.argument("model_name")
+def quantiles(data_name, label, tau1, tau2, model_name):
+
     x, y = load_data("data/"+data_name)
 
-    index = sys.argv[1]
-    tau1 = float(sys.argv[2])
-    tau2 = float(sys.argv[3])
-
-    mod = import_module(sys.argv[4])
-    model = mod.model 
+    mod = import_module(model_name)
+    model = mod.model
 
     # model 1 - upper bound
     loss = CustomLoss(tau1)
     model1 = fit_n(5, model, loss.loss, x, y)
     ym1 = model1.predict(x)
-    model1.save("mlp_{}_{}_{}.h5".format(data_name, index, tau1))
-    np.save("mlp_{}_{}_y_{}".format(data_name, index, tau1), ym1)
+    model1.save("mlp_{}_{}_{}.h5".format(data_name, label, tau1))
+    np.save("mlp_{}_{}_y_{}".format(data_name, label, tau1), ym1)
 
     # model 2 - lower bound
     loss = CustomLoss(tau2)
     model2 = fit_n(5, model, loss.loss, x, y)
     ym2 = model2.predict(x)
-    model2.save("mlp_{}_{}_{}.h5".format(data_name, index, tau2))
-    np.save("mlp_{}_{}_y_{}".format(data_name, index, tau2), ym2)
+    model2.save("mlp_{}_{}_{}.h5".format(data_name, label, tau2))
+    np.save("mlp_{}_{}_y_{}".format(data_name, label, tau2), ym2)
+
+
+@test.command()
+def crossvalidation():
+    click.echo("hello crossvalidation")
+
+
+@test.command()
+@click.option("--label", default="test",
+              help="Label to prepend to output file names.")
+@click.argument("data_name")
+@click.argument("model_name")
+def simple_model(label, data_name, model_name):
+
+    x, y = load_data("data/"+data_name)
+
+    mod = import_module(model_name)
+    model = mod.model
+
+    # model
+    trained_model = fit_n(5, model, losses.mean_squared_error, x, y)
+    ym1 = trained_model.predict(x)
+
+    trained_model.save("mlp_{data}_{label}_plain.h5".format(
+        data=data_name, label=label))
+    np.save("mlp_{data}_{label}_y_plain".format(
+        data=data_name, label=label), ym1)
+
+    # calculate final loss
+    ym1 = ym1.squeeze()
+    print("MSE:  ", mean_squared_error(y, ym1))
+    print("TMSE: ", trimmed_mean_squared_error(y, ym1))
+
+
+@test.command()
+@click.option("--label", default="test",
+              help="Label to prepend to output file names.")
+@click.argument("data_name")
+@click.argument("lower_name")
+@click.argument("upper_name")
+@click.argument("model_name")
+def trimmed_model(data_name, label, lower_name, upper_name, model_name):
+
+    lower = np.load(lower_name)
+    upper = np.load(upper_name)
+
+    x1, y1 = load_data("data/"+data_name)
+    x, y = load_data("data/"+data_name, lower, upper)
+
+    mod = import_module(model_name)
+    model = mod.model
+
+    # model
+    trained_model = fit_n(5, model, losses.mean_squared_error, x, y)
+    ym1 = trained_model.predict(x1)
+
+    trained_model.save(
+        "mlp_{data}_{label}_restricted_{lower}_{upper}.h5".format(
+            data=data_name,
+            label=label,
+            lower=lower_name,
+            upper=upper_name))
+    np.save("mlp_{data}_{label}_y_restricted_{lower}_{upper}".format(
+        data=data_name, label=label, lower=lower_name, upper=upper_name),
+        ym1)
+
+    # calculate final loss
+    ym1 = ym1.squeeze()
+    print("MSE:  ", mean_squared_error(y1, ym1))
+    print("TMSE: ", trimmed_mean_squared_error(y1, ym1))
+
+
+if __name__ == "__main__":
+    test()
